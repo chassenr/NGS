f128d87cd00b853c87e6c2d464525ca1d4317b19
Christiane Hassenrück <chassenr@mpi-bremen.de>
2016-02-22 17:59:28 +0100

    bug fix in efetch_submission_new.sh

--- META_G_T/efetch_submission_new.sh
+++ META_G_T/efetch_submission_new.sh
@@ -6,46 +6,53 @@
 
 if [ -f ${output}.efetch ]
 then
- rm ${output}.efetch
+  rm ${output}.efetch
 fi
 
 while read line
 do
- echo ${line} > ${line}.uid.tmp
- efetch -db protein -id ${line} -mode xml | grep "<OrgName_lineage>" | head -1 > ${line}.efetch.tmp
- paste ${line}.efetch.tmp ${line}.uid.tmp >> ${output}.efetch
- rm ${line}.efetch.tmp
- rm ${line}.uid.tmp
+  echo ${line} > ${line}.uid.tmp
+  efetch -db protein -id ${line} -mode xml | grep "<OrgName_lineage>" | head -1 > ${line}.efetch.tmp
+  paste ${line}.efetch.tmp ${line}.uid.tmp >> ${output}.efetch
+  rm ${line}.efetch.tmp
+  rm ${line}.uid.tmp
 done < ${input}.uid
-sed -e 's/^ *//' -e 's/<OrgName_lineage>//' -e 's/<\/OrgName_lineage>//' ${output}.efetch > ${output}.txt
 
+sed -e 's/^ *//' -e 's/<OrgName_lineage>//' -e 's/<\/OrgName_lineage>//' ${output}.efetch > ${output}.txt
 
 if [ $(cut -f1 ${output}.txt | sed '/^$/d' | wc -l) -ne $(cut -f2 ${output}.txt | sed '/^$/d' | wc -l) ]
 then
- mv ${output}.txt ${output}.missing.txt
- awk 'BEGIN {FS="\t"}  $1!="" {print}' ${output}.missing.txt > ${output}.txt
- 
- until [ $(ls -l ${output}.missing.txt | cut -d' ' -f5) -eq $(ls -l ${output}.txt | cut -d' ' -f5) ]
- do
-  if [ -f tmp.efetch ]
-  then
-   rm tmp.efetch
-  fi
-
-  awk 'BEGIN {FS="\t"}  $1=="" {print}' ${output}.missing.txt | cut -f2 > repeat.uid
   
-  while read line
+  until [ $(cut -f1 ${output}.txt | sed '/^$/d' | wc -l) -eq $(cut -f2 ${output}.txt | sed '/^$/d' | wc -l) ]
   do
-   echo ${line} > ${line}.uid.tmp
-   efetch -db protein -id ${line} -mode xml | grep "<OrgName_lineage>" | head -1 > ${line}.efetch.tmp
-   paste ${line}.efetch.tmp ${line}.uid.tmp >> tmp.efetch
-   rm ${line}.efetch.tmp
-   rm ${line}.uid.tmp
-  done < repeat.uid
+
+    mv ${output}.txt ${output}.tmp.txt
+    awk 'BEGIN {FS="\t"}  $1=="" {print}' ${output}.tmp.txt | cut -f2 > repeat.uid
+    awk 'BEGIN {FS="\t"}  $1!="" {print}' ${output}.tmp.txt > ${output}.txt
+
+    if [ -f repeat.efetch ]
+    then
+      rm repeat.efetch
+    fi
+
+    while read line
+    do
+      echo ${line} > ${line}.uid.tmp
+      efetch -db protein -id ${line} -mode xml | grep "<OrgName_lineage>" | head -1 > ${line}.efetch.tmp
+      paste ${line}.efetch.tmp ${line}.uid.tmp >> repeat.efetch
+      rm ${line}.efetch.tmp
+      rm ${line}.uid.tmp
+    done < repeat.uid
   
-  sed -e 's/^ *//' -e 's/<OrgName_lineage>//' -e 's/<\/OrgName_lineage>//' tmp.efetch >> ${output}.txt
- done
+    sed -e 's/^ *//' -e 's/<OrgName_lineage>//' -e 's/<\/OrgName_lineage>//' repeat.efetch > repeat.txt
+    cat repeat.txt >> ${output}.txt
+
+    awk 'BEGIN {FS="\t"}  $1=="" {print}' repeat.txt | cut -f2 > left.uid
+    
+    if [ $(cat left.uid | wc -l ) -eq $(cat repeat.uid | wc -l) ]
+    then
+      break
+    fi
+
+  done
 fi
-
-
-
